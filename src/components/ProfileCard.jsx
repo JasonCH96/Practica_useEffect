2b18194908089f8714670f634c3abc394034c0a4
Jason Castro <jasoncastroh@gmail.com>
2023-08-11 13:06:57 -0600

    Practica Final

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,36 @@
+import '../components/ProfileCard.css'
+import React, { useEffect, useState } from "react";
+import generateRandomUsersArray from '../data/users'
+
+
+const ProfileCard = (props) => {
+    const [users, setUsers] = useState([]);
+
+    const generateUsers = () => {
+        const randomUsers = generateRandomUsersArray(props.user); 
+        setUsers(randomUsers);
+    };
+
+    useEffect(() => {
+        generateUsers();
+
+    }, []);
+
+    return (
+        <div className='ProfileCard'>
+            <h1>User</h1>
+            <ul>
+                {users.map((user, index) => (
+                    <li key={index}><strong>Name:</strong> {user.name},
+                    <li key={index}></li><strong>Age:</strong> {user.age},
+                    <li key={index}></li><strong>Hobby:</strong> {user.hobby}
+                    </li>
+                ))}
+                <button onClick={users}>Refrescar Tarjetas</button>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default ProfileCard
